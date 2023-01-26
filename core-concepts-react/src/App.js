c9c60531182a35f2ad05348db976438b83295e54
dibya61 <dibyajyotihore.djh16@gmail.com>
2023-01-27 05:46:20 +0600

    first commit

---
+++ core-concepts-react/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var person1="Dibya";
+  var person2={
+    name:"Salman",
+    age:"21"  
+}
+var style1={
+  color:"red",
+  backgroundColor:"blue"
+}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2 style={style1} >My name is:{" "}{ person1}</h2>
+        <h2 style={{backgroundColor:"red",color:"cyan"}}>My name is: {person2.name} And Age is: {person2.age}</h2>
+        
+
+        <h3>Total Number:{2+3}</h3>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
